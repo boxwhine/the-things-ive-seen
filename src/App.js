25ce1b35acf40fb8af925c37a9166a2b0d3a6776
Jesse Rinehart <jrinehart@tune.com>
2018-05-08 00:21:38 -0700

    Update boilerplate text in template

--- src/App.js
+++ src/App.js
@@ -8,12 +8,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Things I've Seen</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Concert/event dashboard and visualization app
         </p>
-        <h1>foo</h1>
       </div>
     );
   }
