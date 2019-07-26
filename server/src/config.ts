bc1613f720cb52c48848e286a766d3b4a7453799
Jesse Rinehart <jesse.rinehart@thoughtworks.com>
2019-07-26 11:32:33 -0500

    Add dev/prod? to config

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,5 +1,6 @@
 const {
   DB_OPTIONS,
+  NODE_ENV,
   POSTGRES_DB,
   POSTGRES_HOST,
   POSTGRES_PASSWORD,
@@ -7,6 +8,8 @@ const {
 } = process.env!;
 
 const config = {
+  isProd: NODE_ENV === 'production',
+  isDev: NODE_ENV !== 'production',
   db: {
     name: POSTGRES_DB!,
     options: !!DB_OPTIONS ? DB_OPTIONS.split('&') : [],
