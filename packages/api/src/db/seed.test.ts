d2238beb8b64428084f4da1fb79148bb6ad3d776
Jesse Rinehart <jesse.r.rinehart@gmail.com>
2023-12-13 19:55:18 -0600

    Move apps into lerna packages, get `npx lerna run test` passing

---
+++ packages/api/src/db/seed.test.ts
@@ -0,0 +1,8 @@
+import { expect, test, vi } from 'vitest';
+// import generateSeedData from './seed';
+
+// @TODO dummy test to prevent '0 tests' failure
+test('DB/Seed', async () => {
+  // const data = await generateSeedData();
+  expect(true).toBe(true);
+});
